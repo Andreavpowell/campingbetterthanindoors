0529155e8907805c5482931ba28c5258391d656e
Andrea Powell <andreavpowell@gmail.com>
2021-08-08 17:15:51 -0700

    added functions to app.js

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-let userName = prompt('Please Enter Your Name: ');
-console.log (userName);
-document.write('<h2>') + userName + '</h2>';
+function getUserName(){
+    let userName = prompt('Please Enter Your Name: ');
+    console.log(userName);
+    document.write('<h2>' + userName + '</h2>');
+    return userName;
+}
 
-let userAnswer = prompt('Do you like camping?');
-console.log (userAnswer)
+function getUserAnswer(){
+    let userAnswer = prompt('Do you like camping?');
+    console.log(userAnswer);
 
-if (userAnswer == 'Yes') {
-    document.write('<h3>Sick. I guess you can stay since you answered \"yes\" to my question, ' + userName + '.' + '</h3>');
-} else {
-    document.write('<h3>' + userName +', your mother and I need to speak with you.</h3>');
+    if (userAnswer.toLowerCase() == 'yes'){
+        document.write('<h3>Sick. I guess you can stay since you answered \"yes\" to my question.' + '</h3>');
+    } else {
+        document.write('<h3>' + 'Your mother and I need to speak with you.</h3>');
+    }
+}
+
+function getLikeHikingAnswer(){
+    let likehiking = prompt('Do you like hiking?');
+
+    if (likehiking.toLowerCase() == 'yes'){
+        document.write('<h3>So. You like hiking. Name one of their albums.</h3>');
+    } else {
+        document.write('<h3>Since you apparently hate everything that\'s great in the world, you should probably go <a href="https://puginarug.com/">here</a> instead. It would be better a use of your time.')
+    }
 }
